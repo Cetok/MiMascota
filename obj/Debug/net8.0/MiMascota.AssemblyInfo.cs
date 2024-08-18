3c6bf487188d9b599729999f0ffa17e3358f3d95
Cetok <jtorvisco42@gmail.com>
2024-08-17 23:03:17 -0500

    cambios finales

--- obj/Debug/net8.0/MiMascota.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiMascota.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiMascota")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3424b71ddcc31f972d31b9444662b4fe0f9f1e19")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiMascota")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiMascota")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
